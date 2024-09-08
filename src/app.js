4c9bb97bf760553fd20be02bc30da89b4d8ef63e
vaishalisjadhav <vaishalijadhav9777@gmail.com>
2024-09-08 20:21:46 +0530

    app.js added

--- src/app.js
+++ src/app.js
@@ -7,3 +7,24 @@ import PostDetails from './pages/postdetails';
 import Header from './components/header';
 import Footer from './components/footer';
 import { BlogProvider } from './context/blogcontext';
+
+
+const App = () => {
+    return (
+      <BlogProvider>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreatePost />} />
+            <Route path="/edit/:id" element={<EditPost />} />
+            <Route path="/post/:id" element={<PostDetails />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </BlogProvider>
+    );
+  };
+  
+  export default App;
+  
